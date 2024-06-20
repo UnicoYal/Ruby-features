7b77ac7e0a7ea65984f0169a8ab25723cad72784
UnicoYal <113387973+UnicoYal@users.noreply.github.com>
2024-06-20 14:47:26 +0300

    File managment (#1)

--- map/map_flatten_vs_flat_map.rb
+++ map/map_flatten_vs_flat_map.rb
@@ -1,18 +1,14 @@
 require 'benchmark/ips'
 
 Benchmark.ips do |bench_mark|
-  list = (0..100000).to_a
+  arr = (0..100000).to_a
 
   bench_mark.report "flat_map" do
-    list.flat_map do |x|
-      [x, x * x]
-    end
+    arr.flat_map { |x| [x, x * x] }
   end
 
   bench_mark.report "map.flatten" do
-    list.map do |x|
-      [x, x * x]
-    end.flatten
+    arr.map { |x| [x, x * x] }.flatten
   end
 
   bench_mark.compare!
